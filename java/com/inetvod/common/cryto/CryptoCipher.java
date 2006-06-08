06cfb9791a64475f53b109d5f268198e9f6a861c
grtvd <grtvd@users.noreply.github.com>
2006-06-08 11:36:49 -0400

    webapp: Updated to store Credit Card in encrypted format using CryptoCipher. Adjusted max lengths of some credit card fields. Updated CreditCard object into internally handle encryption/decryption. Updated CryptoCipher to use Base64 instead of Hex string for encrypted text format.

--- java/com/inetvod/common/cryto/CryptoCipher.java
+++ java/com/inetvod/common/cryto/CryptoCipher.java
@@ -4,23 +4,23 @@
  */
 package com.inetvod.common.cryto;
 
-import java.security.NoSuchAlgorithmException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.io.UnsupportedEncodingException;
 
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.SecretKey;
 import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
 
 import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.codec.DecoderException;
+
+import com.inetvod.common.core.StrUtil;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 public class CryptoCipher
 {
@@ -29,8 +29,7 @@ public class CryptoCipher
 	private static final String ALGORITHM = "PBEWithMD5AndDES";
 	private static final String CHAR_ENCODING = "UTF-8";
 	@SuppressWarnings({"MagicNumber"})
-	private static final byte[] DEFAULT_SALT = { 0x2a, (byte)0x9c, (byte)0xd9, (byte)0xf6, (byte)0x85,
-		(byte)0xe3, (byte)0xdd, (byte)0x29 };
+	private static final String DEFAULT_SALT = "2a9cd9f685e3dd29";
 	private static final int DEFAULT_ITERATIONCOUNT = 1000;
 
 	private byte[] fSalt;
@@ -42,21 +41,19 @@ public class CryptoCipher
 	private Cipher fEncryptCipher;
 	private Cipher fDecryptCipher;
 
-	private CryptoCipher(String encryptionPassword)
+	private CryptoCipher(String encryptionPassword) throws Exception
 	{
-		fSalt = DEFAULT_SALT;
-		fIterationCount = DEFAULT_ITERATIONCOUNT;
-		fEncryptionPassword = encryptionPassword.toCharArray();
+		this(DEFAULT_SALT, DEFAULT_ITERATIONCOUNT, encryptionPassword);
 	}
 
-	private CryptoCipher(String salt, int iterationCount, String encryptionPassword) throws DecoderException
+	private CryptoCipher(String salt, int iterationCount, String encryptionPassword) throws Exception
 	{
 		fSalt = Hex.decodeHex(salt.toCharArray());
 		fIterationCount = iterationCount;
 		fEncryptionPassword = encryptionPassword.toCharArray();
 	}
 
-	public CryptoCipher newInstance(String encryptionPassword)
+	public static CryptoCipher newInstance(String encryptionPassword) throws Exception
 	{
 		return new CryptoCipher(encryptionPassword);
 	}
@@ -112,22 +109,27 @@ public class CryptoCipher
 		return fDecryptCipher;
 	}
 
-	public String encrypt(String textToEncrypt) throws NoSuchAlgorithmException, InvalidKeySpecException,
-		NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException,
-		IllegalBlockSizeException, UnsupportedEncodingException
+	public String encrypt(String textToEncrypt) throws Exception
 	{
+		if(!StrUtil.hasLen(textToEncrypt))
+			return null;
+
 		byte[] cleartext = textToEncrypt.getBytes(CHAR_ENCODING);
 		byte[] ciphertext = getEncryptCipher().doFinal(cleartext);
-		char[] hexText = Hex.encodeHex(ciphertext);
 
-		return new String(hexText);
+		return (new BASE64Encoder()).encode(ciphertext);
+		// to return as Hex bytes in String format use:
+		//return new String(Hex.encodeHex(ciphertext));
 	}
 
-	public String decrypt(String encryptedText) throws DecoderException, NoSuchAlgorithmException,
-		InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, InvalidKeySpecException,
-		BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException
+	public String decrypt(String encryptedText) throws Exception
 	{
-		byte[] ciphertext = Hex.decodeHex(encryptedText.toCharArray());
+		if(!StrUtil.hasLen(encryptedText))
+			return null;
+
+		// if stored Hex bytes in String format use:
+		//byte[] ciphertext = Hex.decodeHex(encryptedText.toCharArray());
+		byte[] ciphertext = (new BASE64Decoder()).decodeBuffer(encryptedText);
 		byte[] cleartext = getDecryptCipher().doFinal(ciphertext);
 
 		return new String(cleartext, CHAR_ENCODING);
--- java/com/inetvod/common/data/CreditCard.java
+++ java/com/inetvod/common/data/CreditCard.java
@@ -9,63 +9,124 @@ import java.lang.reflect.Constructor;
 import com.inetvod.common.core.DataReader;
 import com.inetvod.common.core.DataWriter;
 import com.inetvod.common.core.Readable;
+import com.inetvod.common.core.StrUtil;
 import com.inetvod.common.core.Writeable;
+import com.inetvod.common.cryto.CryptoCipher;
 
 public class CreditCard implements Readable, Writeable
 {
 	/* Constants */
 	public static Constructor<CreditCard> CtorDataReader = DataReader.getCtor(CreditCard.class);
+	public static final String KeyPassword = "com.inetvod.common.data.CreditCard";
 
-	private static final int NameOnCCMaxLength = 64;
-	private static final int CCNumberMaxLength = 32;
-	private static final int CCSICMaxLength = 16;
-	private static final int ExpireDateMaxLength = 6;
+	private static final int NameOnCCMaxLength = 64;	// max encrypted length
+	private static final int CCNumberMaxLength = 32;	// max encrypted length
+	private static final int CCSICMaxLength = 16;		// max encrypted length
+	private static final int ExpireDateMaxLength = 32;	// max encrypted length
 
 	private static final int Century = 100;
 	private static final int DefaultMillennia = 2000;
 
 	/* Fields */
-	private String fNameOnCC;
+	private CryptoCipher fCryptoCipher;		// don't access directory, use getCryptoCipher()
+
+	private String fNameOnCCEncrypted;
+	private String fNameOnCCPlainText;
 	private CreditCardType fCCType;
-	private String fCCNumber;
-	private String fCCSIC;
-	private String fExpireDate;
+	private String fCCNumberEncrypted;
+	private String fCCNumberPlainText;
+	private String fCCSICEncrypted;
+	private String fCCSICPlainText;
+	private String fExpireDateEncrypted;
+	private String fExpireDatePlainText;
 	private Address fBillingAddress;
 
 	/* Getters and Setters */
-	public String getNameOnCC() { return fNameOnCC; }
-	public void setNameOnCC(String nameOnCC) { fNameOnCC = nameOnCC; }
+	private CryptoCipher getCryptoCipher() throws Exception
+	{
+		if(fCryptoCipher == null)
+			fCryptoCipher = CryptoCipher.newInstance(KeyPassword);	//TODO: get KeyPassword from CryptoKeyStore
+		return fCryptoCipher;
+	}
+
+	public String getNameOnCC() throws Exception
+	{
+		if(StrUtil.hasLen(fNameOnCCEncrypted) && !StrUtil.hasLen(fNameOnCCPlainText))
+			fNameOnCCPlainText = getCryptoCipher().decrypt(fNameOnCCEncrypted);
+		return fNameOnCCPlainText;
+	}
+
+	public void setNameOnCC(String nameOnCC) throws Exception
+	{
+		fNameOnCCPlainText = nameOnCC;
+		fNameOnCCEncrypted = getCryptoCipher().encrypt(fNameOnCCPlainText);
+	}
 
 	public CreditCardType getCCType() { return fCCType; }
 	public void setCCType(CreditCardType CCType) { fCCType = CCType; }
 
-	public String getCCNumber() { return fCCNumber; }
-	public void setCCNumber(String CCNumber) { fCCNumber = CCNumber; }
-
-	public String getCCSIC() { return fCCSIC; }
-	public void setCCSIC(String CCSIC) { fCCSIC = CCSIC; }
-
-	public int getExpireDateMonth()
+	public String getCCNumber() throws Exception
 	{
-		if((fExpireDate == null) || (fExpireDate.length() != 6))
-			return 0;
-
-		return Integer.parseInt(fExpireDate.substring(0, 2));
+		if(StrUtil.hasLen(fCCNumberEncrypted) && !StrUtil.hasLen(fCCNumberPlainText))
+			fCCNumberPlainText = getCryptoCipher().decrypt(fCCNumberEncrypted);
+		return fCCNumberPlainText;
 	}
 
-	public int getExpireDateYear()
+	public void setCCNumber(String ccNumber) throws Exception
 	{
-		if((fExpireDate == null) || (fExpireDate.length() != 6))
-			return 0;
-
-		return Integer.parseInt(fExpireDate.substring(2, 6));
+		fCCNumberPlainText = ccNumber;
+		fCCNumberEncrypted = getCryptoCipher().encrypt(fCCNumberPlainText);
 	}
 
-	public void setExpireDate(int expireMonth, int expireYear)
+	public String getCCSIC() throws Exception
+	{
+		if(StrUtil.hasLen(fCCSICEncrypted) && !StrUtil.hasLen(fCCSICPlainText))
+			fCCSICPlainText = getCryptoCipher().decrypt(fCCSICEncrypted);
+		return fCCSICPlainText;
+	}
+
+	public void setCCSIC(String ccSIC) throws Exception
+	{
+		fCCSICPlainText = ccSIC;
+		fCCSICEncrypted = getCryptoCipher().encrypt(fCCSICPlainText);
+	}
+
+	private String getExpireDate() throws Exception
+	{
+		if(StrUtil.hasLen(fExpireDateEncrypted) && !StrUtil.hasLen(fExpireDatePlainText))
+			fExpireDatePlainText = getCryptoCipher().decrypt(fExpireDateEncrypted);
+		return fExpireDatePlainText;
+	}
+
+	private void setExpireDate(String expireDate) throws Exception
+	{
+		fExpireDatePlainText = expireDate;
+		fExpireDateEncrypted = getCryptoCipher().encrypt(fExpireDatePlainText);
+	}
+
+	public int getExpireDateMonth() throws Exception
+	{
+		String expireDate = getExpireDate();
+		if((expireDate == null) || (expireDate.length() != 6))
+			return 0;
+
+		return Integer.parseInt(expireDate.substring(0, 2));
+	}
+
+	public int getExpireDateYear() throws Exception
+	{
+		String expireDate = getExpireDate();
+		if((expireDate == null) || (expireDate.length() != 6))
+			return 0;
+
+		return Integer.parseInt(expireDate.substring(2, 6));
+	}
+
+	public void setExpireDate(int expireMonth, int expireYear) throws Exception
 	{
 		if(expireYear < Century)
 			expireYear += DefaultMillennia;
-		fExpireDate = String.format("%02d%04d", expireMonth, expireYear);
+		setExpireDate(String.format("%02d%04d", expireMonth, expireYear));
 	}
 
 	public Address getBillingAddress() { return fBillingAddress; }
@@ -84,21 +145,21 @@ public class CreditCard implements Readable, Writeable
 	/* Implementation */
 	public void readFrom(DataReader reader) throws Exception
 	{
-		fNameOnCC = reader.readString("NameOnCC", NameOnCCMaxLength);
+		fNameOnCCEncrypted = reader.readString("NameOnCC", NameOnCCMaxLength);
 		fCCType = CreditCardType.convertFromString(reader.readString("CCType", CreditCardType.MaxLength));
-		fCCNumber = reader.readString("CCNumber", CCNumberMaxLength);
-		fCCSIC = reader.readString("CCSIC", CCSICMaxLength);
-		fExpireDate = reader.readString("ExpireDate", ExpireDateMaxLength);
+		fCCNumberEncrypted = reader.readString("CCNumber", CCNumberMaxLength);
+		fCCSICEncrypted = reader.readString("CCSIC", CCSICMaxLength);
+		fExpireDateEncrypted = reader.readString("ExpireDate", ExpireDateMaxLength);
 		fBillingAddress = reader.readObject("BillingAddress", Address.CtorDataReader);
 	}
 
 	public void writeTo(DataWriter writer) throws Exception
 	{
-		writer.writeString("NameOnCC", fNameOnCC, NameOnCCMaxLength);
+		writer.writeString("NameOnCC", fNameOnCCEncrypted, NameOnCCMaxLength);
 		writer.writeString("CCType", CreditCardType.convertToString(fCCType), CreditCardType.MaxLength);
-		writer.writeString("CCNumber", fCCNumber, CCNumberMaxLength);
-		writer.writeString("CCSIC", fCCSIC, CCSICMaxLength);
-		writer.writeString("ExpireDate", fExpireDate, ExpireDateMaxLength);
+		writer.writeString("CCNumber", fCCNumberEncrypted, CCNumberMaxLength);
+		writer.writeString("CCSIC", fCCSICEncrypted, CCSICMaxLength);
+		writer.writeString("ExpireDate", fExpireDateEncrypted, ExpireDateMaxLength);
 		writer.writeObject("BillingAddress", fBillingAddress);
 	}
 }
