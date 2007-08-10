19e66cb50de189f9e4cd5944f3b4149002b8f381
grtvd <grtvd@users.noreply.github.com>
2007-08-10 12:46:15 -0400

    shared: Converted MediaMIME to enum.

--- java/com/inetvod/common/data/MediaMIME.java
+++ java/com/inetvod/common/data/MediaMIME.java
@@ -1,15 +1,62 @@
 /**
- * Copyright © 2006 iNetVOD, Inc. All Rights Reserved.
+ * Copyright © 2006-2007 iNetVOD, Inc. All Rights Reserved.
  * iNetVOD Confidential and Proprietary.  See LEGAL.txt.
  */
 package com.inetvod.common.data;
 
-public class MediaMIME
+import java.util.HashMap;
+
+import com.inetvod.common.core.Logger;
+
+public enum MediaMIME
 {
-	public static final String video_x_ms_wmv = "video/x-ms-wmv";
-	public static final String video_x_msvideo = "video/x-msvideo";
-	public static final String video_mp4 = "video/mp4";
-	public static final String video_mov = "video/mov";
-	public static final String video_quicktime = "video/quicktime";
-	public static final String audio_mpeg = "audio/mpeg";
+	video_x_ms_wmv("video/x-ms-wmv"),
+	video_x_msvideo("video/x-msvideo"),
+	video_mp4("video/mp4"),
+	video_mov("video/mov"),
+	video_quicktime("video/quicktime"),
+	audio_mpeg("audio/mpeg");
+
+	/* Constants */
+	public static final int MaxLength = 32;
+
+	/* Fields */
+	private static HashMap<String, MediaMIME> fAllValues = new HashMap<String, MediaMIME>();
+
+	private final String fValue;
+
+	static
+	{
+		for(MediaMIME value : values())
+			fAllValues.put(value.toString(), value);
+	}
+
+	/* Getters and Setters */
+	public String toString() { return fValue; }
+
+	/* Construction */
+	private MediaMIME(String name)
+	{
+		fValue = name;
+	}
+
+	public static MediaMIME convertFromString(String value)
+	{
+		if((value == null) || (value.length() == 0))
+			return null;
+
+		MediaMIME mediaMIME = fAllValues.get(value);
+		if(mediaMIME != null)
+			return mediaMIME;
+
+		Logger.logWarn(MediaMIME.class, "convertFromString", String.format("bad value(%s)", value));
+		return null;
+	}
+
+	public static String convertToString(MediaMIME value)
+	{
+		if(value == null)
+			return null;
+		return value.toString();
+	}
 }
