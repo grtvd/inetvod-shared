31dbe0760c42ecc8af60b3d26acef9454993fa80
grtvd <grtvd@users.noreply.github.com>
2007-08-16 23:05:56 -0400

    contmgr: Updated to MediaMapper to support Video/AudioCodec to MediaMIME.  Update MainApp.determineContentInfo() to set MediaMIME.

--- java/com/inetvod/contmgr/data/MediaMapper.java
+++ java/com/inetvod/contmgr/data/MediaMapper.java
@@ -8,34 +8,46 @@ import java.util.HashMap;
 
 import com.inetvod.common.core.FileExtension;
 import com.inetvod.common.core.Logger;
+import com.inetvod.common.data.MediaMIME;
 
 public class MediaMapper
 {
 	/* Fields */
-	private static HashMap<VideoCodec, FileExtension> fMapVideoCodec;
-	private static HashMap<AudioCodec, FileExtension> fMapAudioCodec;
+	private static HashMap<VideoCodec, FileExtension> fVideoCodecFileExtensionMap;
+	private static HashMap<AudioCodec, FileExtension> fAudioCodecFileExtensionMap;
+
+	private static HashMap<VideoCodec, MediaMIME> fVideoCodecMediaMIMEMap;
+	private static HashMap<AudioCodec, MediaMIME> fAudioCodecMediaMIMEMap;
 
 	/* Construction */
 	static
 	{
-		fMapVideoCodec = new HashMap<VideoCodec, FileExtension>();
-		fMapVideoCodec.put(VideoCodec.WMV1, FileExtension.wmv);
-		fMapVideoCodec.put(VideoCodec.WMV2, FileExtension.wmv);
-		fMapVideoCodec.put(VideoCodec.WMV3, FileExtension.wmv);
-		fMapVideoCodec.put(VideoCodec.AVC1, FileExtension.mp4);
-		fMapVideoCodec.put(VideoCodec.MP4V, FileExtension.mp4);
-		fMapVideoCodec.put(VideoCodec.SVQ3, FileExtension.mov);
+		fVideoCodecFileExtensionMap = new HashMap<VideoCodec, FileExtension>();
+		fVideoCodecFileExtensionMap.put(VideoCodec.WMV1, FileExtension.wmv);
+		fVideoCodecFileExtensionMap.put(VideoCodec.WMV2, FileExtension.wmv);
+		fVideoCodecFileExtensionMap.put(VideoCodec.WMV3, FileExtension.wmv);
+		fVideoCodecFileExtensionMap.put(VideoCodec.AVC1, FileExtension.mp4);
+		fVideoCodecFileExtensionMap.put(VideoCodec.MP4V, FileExtension.mp4);
+		fVideoCodecFileExtensionMap.put(VideoCodec.SVQ3, FileExtension.mov);
 
-		fMapAudioCodec = new HashMap<AudioCodec, FileExtension>();
-		fMapAudioCodec.put(AudioCodec.MP3, FileExtension.mp3);
-		fMapAudioCodec.put(AudioCodec.M4A, FileExtension.m4a);
-		fMapAudioCodec.put(AudioCodec.WMA2, FileExtension.wma);
+		fAudioCodecFileExtensionMap = new HashMap<AudioCodec, FileExtension>();
+		fAudioCodecFileExtensionMap.put(AudioCodec.MP3, FileExtension.mp3);
+		fAudioCodecFileExtensionMap.put(AudioCodec.M4A, FileExtension.m4a);
+		fAudioCodecFileExtensionMap.put(AudioCodec.WMA2, FileExtension.wma);
+
+		fVideoCodecMediaMIMEMap = new HashMap<VideoCodec, MediaMIME>();
+		fVideoCodecMediaMIMEMap.put(VideoCodec.WMV1, MediaMIME.video_x_ms_wmv);
+		fVideoCodecMediaMIMEMap.put(VideoCodec.WMV2, MediaMIME.video_x_ms_wmv);
+		fVideoCodecMediaMIMEMap.put(VideoCodec.WMV3, MediaMIME.video_x_ms_wmv);
+
+		fAudioCodecMediaMIMEMap = new HashMap<AudioCodec, MediaMIME>();
+		fAudioCodecMediaMIMEMap.put(AudioCodec.MP3, MediaMIME.audio_mpeg);
 	}
 
 	/* Implementation */
 	public static FileExtension getDefaultFileExtension(VideoCodec videoCodec)
 	{
-		FileExtension fileExtension = fMapVideoCodec.get(videoCodec);
+		FileExtension fileExtension = fVideoCodecFileExtensionMap.get(videoCodec);
 
 		if(fileExtension == null)
 			Logger.logWarn(MediaMapper.class, "getDefaultFileExtension", String.format("No map for VideoCodec(%s)",
@@ -46,12 +58,34 @@ public class MediaMapper
 
 	public static FileExtension getDefaultFileExtension(AudioCodec audioCodec)
 	{
-		FileExtension fileExtension = fMapAudioCodec.get(audioCodec);
+		FileExtension fileExtension = fAudioCodecFileExtensionMap.get(audioCodec);
 
 		if(fileExtension == null)
 			Logger.logWarn(MediaMapper.class, "getDefaultFileExtension", String.format("No map for AudioCodec(%s)",
 				AudioCodec.convertToString(audioCodec)));
 
 		return fileExtension;
+	}
+
+	public static MediaMIME getDefaultMediaMIME(VideoCodec videoCodec)
+	{
+		MediaMIME mediaMIME = fVideoCodecMediaMIMEMap.get(videoCodec);
+
+		if(mediaMIME == null)
+			Logger.logWarn(MediaMapper.class, "getDefaultMediaMIME", String.format("No map for VideoCodec(%s)",
+				VideoCodec.convertToString(videoCodec)));
+
+		return mediaMIME;
+	}
+
+	public static MediaMIME getDefaultMediaMIME(AudioCodec audioCodec)
+	{
+		MediaMIME mediaMIME = fAudioCodecMediaMIMEMap.get(audioCodec);
+
+		if(mediaMIME == null)
+			Logger.logWarn(MediaMapper.class, "getDefaultMediaMIME", String.format("No map for AudioCodec(%s)",
+				AudioCodec.convertToString(audioCodec)));
+
+		return mediaMIME;
 	}
 }
